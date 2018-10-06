1656cf1503099b433084057b2bfef51452b157a9
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2018-10-06 00:17:37 -0400

    commit2

--- src/KellyFang/KFangLib.java
+++ src/KellyFang/KFangLib.java
@@ -54,23 +54,36 @@ public class KFangLib {
         return sum;
     }
 
-    public static int leastCommonMultiple(int num1, int num2, int num3) //incomplete
+    public static String datestr(String str)
+    {
+        String date = str.substring(3,5);
+        String month = str.substring(0,2);
+        String year = str.substring(6,10);
+        String result = date+"-"+month+"-"+year;
+        return result;
+    }
+
+   /* public static int leastCommonMultiple(int num1, int num2, int num3) //incomplete
     {
         int lcm = 0;
         if ((num1%num2 == 0)&&(num1%num3 == 0)) {
             lcm = num1;
         }
-        if ((num2%num1 == 0)&&(num2%num3 == 0)) {
+        else if ((num2%num1 == 0)&&(num2%num3 == 0)) {
             lcm = num2;
         }
-        if ((num3%num1 == 0)&&(num3%num2 == 0)) {
+        else if ((num3%num1 == 0)&&(num3%num2 == 0)) {
             lcm = num3;
         }
         return lcm;
-    }
-
-   // public static int stringUnion(String str1, String str2, String str3)
+    }**/
+    public static int stringUnion(String str1, String str2, String str3)
     {
+        int length1 = str1.length();
+        int length2 = str2.length();
+        int length3 = str3.length();
+        int count = 0;
+
 
 
     }
--- src/KellyFang/Main.java
+++ src/KellyFang/Main.java
@@ -8,7 +8,9 @@ public class Main {
         System.out.println(KFangLib.isFibonacci(5));
         System.out.println(KFangLib.isPalindrome("mom"));
         System.out.println(KFangLib.sumUpTo(3));
-        System.out.println(KFangLib.leastCommonMultiple(2,4,6));
+        System.out.println(KFangLib.datestr("06/22/2002"));
+        System.out.println(KFangLib.stringUnion());
+        //System.out.println(KFangLib.leastCommonMultiple(2,4,6));
 
     }
 }
